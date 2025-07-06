33a90073bc087e38002f5ba59e8c30608d8064ad
SaiPrasannaRaoKotte <119796277+KSPRAO@users.noreply.github.com>
2025-07-06 19:21:02 +0530

    Create RegionRepository.java

---
+++ Week_3/Springdatajpa_MySQL/RegionRepository.java
@@ -0,0 +1,9 @@
+package com.library.repository;
+
+import com.library.model.Region;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RegionRepository extends JpaRepository<Region, String> {
+}
